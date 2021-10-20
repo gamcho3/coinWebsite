c7169853c8110906fe003bbe4f3dc2dbb8b25b26
Yong Jin <70016523+gamcho3@users.noreply.github.com>
2021-10-20 22:26:22 +0900

    edit cryptoList page

--- src/components/Cryptocurrencies.js
+++ src/components/Cryptocurrencies.js
@@ -1,22 +1,42 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import { Card, Row, Col, Input, Spin } from "antd";
 import { useGetCryptosQuery } from "../services/cryptoApi";
 import { useGetExchangeQuery } from "../services/exchangeApi";
 import { Link } from "react-router-dom";
 import { numToKorean } from "num-to-korean";
+import classes from "./Cryptocurrencies.module.css";
+const { Search } = Input;
 const Cryptocurrencies = ({ simplified }) => {
   const count = simplified ? 10 : 100;
   const { data: cryptoList, isFetching } = useGetCryptosQuery(count);
   const { data: exchangeData } = useGetExchangeQuery();
   const [cryptos, setCryptos] = useState(cryptoList?.data?.coins);
+  const [searchCoin, setSearchCoin] = useState("");
   const koreanExchange = exchangeData?.rates?.KRW;
+
+  useEffect(() => {
+    const filteredData = cryptoList?.data?.coins.filter((coin) =>
+      coin.name.toLowerCase().includes(searchCoin.toLowerCase())
+    );
+    setCryptos(filteredData);
+  }, [cryptoList, searchCoin]);
 
   if (isFetching) return <Spin tip="loading..." />;
 
   return (
     <>
+      {!simplified && (
+        <div className={classes["crypto-search"]}>
+          <Search
+            placeholder="search coin"
+            enterButton
+            onChange={(e) => setSearchCoin(e.target.value)}
+          />
+        </div>
+      )}
+
       <Row gutter={[32, 32]}>
-        {cryptos.map((crypto) => {
+        {cryptos?.map((crypto) => {
           return (
             <Col xs={24} sm={12} lg={6} key={crypto.uuid}>
               <Link to={`/crypto:${crypto.id}`}>
